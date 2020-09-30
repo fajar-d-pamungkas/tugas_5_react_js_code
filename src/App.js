435d61be2b9fe1d8e7f74895bbf9336ee7bc0629
fajar-d-pamungkas <71364634+fajar-d-pamungkas@users.noreply.github.com>
2020-09-30 12:55:36 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import MenuMakanan from './Page/MenuMakanan'
+
+function App() {
+  return (
+    <div>
+      <h6>TUGAS 5 REACT JS</h6>
+      <MenuMakanan/>
+    </div>
+  );
+}
+
+export default App;
